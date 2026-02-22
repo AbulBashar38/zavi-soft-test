da287ad52ff15fed95a0c2f8ce344463a2ebd8f5
basar <abulbasarofficial5403@gmail.com>
2026-02-22 14:31:14 +0600

    allow all source for the image

--- next.config.ts
+++ next.config.ts
@@ -5,47 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.imgur.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placeimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "trial.co.id",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "api.lorem.space",
-      },
-      {
-        protocol: "https",
-        hostname: "pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "image2.com",
-      },
-      {
-        protocol: "https",
-        hostname: "api.escuelajs.co",
+        hostname: "**",
       },
     ],
   },
